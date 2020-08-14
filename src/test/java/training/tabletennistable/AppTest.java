191ab63eefc3ccddb6215f41057cc01dab57efd5
Luan Kirkwood <luan.kirkwood@aexp.com>
2020-08-14 13:48:51 +0100

    tests

--- src/test/java/training/tabletennistable/AppTest.java
+++ src/test/java/training/tabletennistable/AppTest.java
@@ -2,7 +2,9 @@ package training.tabletennistable;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -10,6 +12,9 @@ import org.mockito.MockitoAnnotations;
 public class AppTest {
     @Mock
     LeagueRenderer renderer;
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
     @Before
     public void initMocks() {
@@ -25,5 +30,54 @@ public class AppTest {
         App app = new App(league, renderer, null);
 
         Assert.assertEquals("Rendered League", app.sendCommand("print"));
+    }
+
+    @Mock
+    League league;
+
+    @Test
+    public void testAddPlayer()
+    {
+        league.addPlayer("Me");
+
+        App app = new App(league, null, null);
+
+        Mockito.verify(league).addPlayer("Me");
+        Assert.assertEquals("Added player Me", app.sendCommand("add player Me"));
+
+    }
+
+    @Test
+    public void testRecordWin()
+    {
+        league.recordWin("Jack", "Jill");
+        App app = new App(league, null, null);
+
+        app.sendCommand("add player Jack");
+        app.sendCommand("add player Jill");
+
+        Assert.assertEquals("Recorded Jack win against Jill", app.sendCommand("record win Jack Jill"));
+
+    }
+
+    @Test
+    public void testWinner()
+    {
+        App app = new App(league, null, null);
+        String response = app.sendCommand("winner");
+        Assert.assertEquals(league.getWinner(), response);
+    }
+
+
+    @Mock
+    FileService fileService;
+
+    @Test
+    public void testSaveGame() {
+        String path = "test/file.csv";
+        App app = new App(league, null, fileService);
+        String response = app.sendCommand("save " + path);
+        Assert.assertEquals("Saved " + path, response);
+        Mockito.verify(fileService).save(path, league);
     }
 }
--- src/test/java/training/tabletennistable/LeagueTest.java
+++ src/test/java/training/tabletennistable/LeagueTest.java
@@ -23,4 +23,77 @@ public class LeagueTest {
         Assert.assertEquals(1, firstRowPlayers.size());
         Assert.assertThat(firstRowPlayers, IsCollectionContaining.hasItem("Bob"));
     }
+
+    @Test
+    public void testGetRows(){
+
+        League league = new League();
+
+        league.addPlayer("Bob");
+        league.addPlayer("Slob");
+
+        List<LeagueRow> rows = league.getRows();
+        Assert.assertEquals("Bob", rows.get(0).getPlayers().get(0));
+        Assert.assertEquals("Slob", rows.get(1).getPlayers().get(0));
+    }
+
+    @Test
+    public void testRecordWin(){ //winner must be one row below
+
+        League league = new League();
+
+        league.addPlayer("Harry");
+        league.addPlayer("Tom");
+        league.addPlayer("Bob");
+        league.addPlayer("Slob");
+
+        league.recordWin("Tom", "Harry");
+
+        List<LeagueRow> rows = league.getRows();
+        Assert.assertEquals("Tom", rows.get(0).getPlayers().get(0));
+        Assert.assertEquals("Harry", rows.get(1).getPlayers().get(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRecordWinWinnerInAnyRow(){ //winner can be in any row
+
+        League league = new League();
+
+        league.addPlayer("Harry");
+        league.addPlayer("Tom");
+        league.addPlayer("Bob");
+        league.addPlayer("Slob");
+
+        league.recordWin("Slob", "Harry");
+
+        List<LeagueRow> rows = league.getRows();
+        Assert.assertEquals("Slob", rows.get(0).getPlayers().get(0));
+        Assert.assertEquals("Harry", rows.get(1).getPlayers().get(0));
+    }
+
+    @Test
+    public void testGetWinner(){
+
+        League league = new League();
+
+        league.addPlayer("Harry");
+        league.addPlayer("Tom");
+        league.addPlayer("Bob");
+        league.addPlayer("Slob");
+
+        Assert.assertEquals("Harry", league.getWinner());
+    }
+
+    @Test
+    public void testGetWinnerPlayerNotInGame(){
+
+        League league = new League();
+
+        league.addPlayer("Harry");
+        league.addPlayer("Tom");
+        league.addPlayer("Bob");
+        league.addPlayer("Slob");
+
+        Assert.assertEquals("Alex", league.getWinner());
+    }
 }
